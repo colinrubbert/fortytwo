8fed9370b42ce3880ce3fa3f03206d98ccd6fa1a
Colin Rubbert <colinrubbert@gmail.com>
2015-05-15 22:10:43 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fortytwo::Application.config.secret_key_base = 'ff4ec6e84249cb789d81f4ba91fdf026ad284ebc3a99e0f6c9fa4c3107b8f50f574b89764880102850d5caa561b843af9d2036d646f686af138d78fec78fa4d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fortytwo::Application.config.session_store :cookie_store, key: '_fortytwo_session'
