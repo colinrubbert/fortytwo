77889625c31616b921bacb69b8dfae5369ffb77f
Colin Rubbert <colinrubbert@gmail.com>
2015-05-29 01:48:04 +0000

    destroy request model, generate new request model, configure request model

--- db/migrate/20150529014708_create_requests.rb
+++ db/migrate/20150529014708_create_requests.rb
@@ -2,6 +2,11 @@ class CreateRequests < ActiveRecord::Migration
   def change
     create_table :requests do |t|
 
+      t.string :name
+      t.text :description
+      t.string :phone
+      t.string :email
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529013552) do
+ActiveRecord::Schema.define(version: 20150529014708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "requests", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "phone"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
