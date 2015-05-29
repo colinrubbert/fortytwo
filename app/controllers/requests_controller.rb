f8cdc99fe60e7b3b7391b47ffb8d40b1c638d3e3
Colin Rubbert <colinrubbert@gmail.com>
2015-05-29 02:20:55 +0000

    Create 'create' method for submitting request form

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -8,4 +8,15 @@ class RequestsController < ApplicationController
     @request = Request.new
   end
 
+  def create
+    Request.create(request_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:name, :description, :phone, :email)
+  end
+
 end
